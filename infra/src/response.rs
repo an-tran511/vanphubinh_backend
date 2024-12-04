96fb004731eaa4c356e6b91f366058f6b3e30cdd
An <an.tran511@outlook.com>
2024-12-04 18:06:42 +0700

    refactor: refactor infra code

--- infra/src/response.rs
+++ infra/src/response.rs
@@ -1,3 +1,4 @@
+use axum::{response::IntoResponse, response::Response, Json};
 use serde::Serialize;
 
 #[derive(Serialize)]
@@ -10,4 +11,29 @@ pub struct ErrorResponse {
 #[derive(Serialize)]
 pub struct OkResponse {
   pub ok: bool,
+}
+
+#[derive(Debug, Clone, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct PaginationMeta {
+  pub page: u64,
+  pub total_pages: u64,
+  pub per_page: u64,
+  pub total: u64,
+}
+
+#[derive(Debug, Clone, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct GetResponse<T> {
+  pub data: Vec<T>,
+  pub meta: PaginationMeta,
+}
+
+impl<T> IntoResponse for GetResponse<T>
+where
+  T: Serialize,
+{
+  fn into_response(self) -> Response {
+    Json(self).into_response()
+  }
 }
--- infra/src/util.rs
+++ infra/src/util.rs
@@ -1,31 +1,5 @@
 use crate::response::{ErrorResponse, OkResponse};
-use axum::{response::IntoResponse, response::Response, Json};
-use serde::Serialize;
-
-#[derive(Debug, Clone, Serialize)]
-#[serde(rename_all = "camelCase")]
-pub struct PaginationMeta {
-  pub page: u64,
-  pub total_pages: u64,
-  pub per_page: u64,
-  pub total: u64,
-}
-
-#[derive(Debug, Clone, Serialize)]
-#[serde(rename_all = "camelCase")]
-pub struct GetResponse<T> {
-  pub data: Vec<T>,
-  pub meta: PaginationMeta,
-}
-
-impl<T> IntoResponse for GetResponse<T>
-where
-  T: Serialize,
-{
-  fn into_response(self) -> Response {
-    Json(self).into_response()
-  }
-}
+use axum::{response::IntoResponse, Json};
 
 pub fn ok() -> impl IntoResponse {
   Json(OkResponse { ok: true }).into_response()
